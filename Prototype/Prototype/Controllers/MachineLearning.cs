e7ea813bba37dffa3ece07840a0b1ef639b3fb63
unknown <Kenneth>
2024-04-10 12:52:13 +0200

    prototype des pages web

---
+++ Prototype/Prototype/Controllers/MachineLearning.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prototype.Controllers
+{
+    public class MachineLearningController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prototype/Prototype/Controllers/Model2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prototype.Controllers
+{
+    public class Model2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
