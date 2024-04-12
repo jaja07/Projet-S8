b38cd82408bc9f5c6d7d7eb4668a9b2bbefd3525
unknown <Kenneth>
2024-04-12 13:00:23 +0200

    J'ai fini les IHM pour le sprint 1

---
+++ Prototype/Prototype/Controllers/Model5.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prototype.Controllers
+{
+    public class Model5 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
